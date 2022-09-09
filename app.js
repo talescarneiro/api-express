031eb5cbb4b49ca9a28e7a6661c4dea880f14335
Tales <jobbtales@gmail.com>
2022-09-08 21:58:11 -0300

    test

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ import usersRoutes from "./src/routes/users.routes.js"
 
 const app = express()
 
-app.use("", usersRoutes)
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Hosting nodejs on netlify"})
+})
 
 const PORT = 3000
 
 app.listen(PORT, () => console.log('Server is running!'))
+
+export default app
